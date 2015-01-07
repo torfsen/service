8a2f8cf0c3b5b177de45fafd53c05bb11d09f9d0
Florian Brucker <mail@florianbrucker.de>
2015-01-07 23:57:40 +0100

    Added tests.

---
+++ runtests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2014 Florian Brucker
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""
+Test runner for ``service`` tests.
+"""
+
+import os.path
+import sys
+
+import nose
+
+_ROOT_DIR = os.path.dirname(__file__)
+_SRC_DIR = os.path.join(_ROOT_DIR, 'src')
+sys.path.insert(0, _SRC_DIR)
+
+
+argv = sys.argv[:]
+argv.insert(1, '--nocapture')  # Messing with STDIN and STDOUT causes problems
+                               # with the ``daemon`` module.
+nose.main(argv=argv)
---
+++ test/test_service.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+"""
+Tests for the ``service`` module.
+"""
+
+import logging
+import threading
+import time
+
+from nose.tools import ok_ as ok
+import psutil
+
+from service import Service
+
+
+_NAME = 'python-service-test-daemon'
+
+
+def is_running():
+    """
+    Check if the test daemon is running.
+    """
+    for process in psutil.process_iter():
+        if process.name() == _NAME:
+            return True
+    return False
+
+
+def assert_running():
+    """
+    Assert that the test daemon is running.
+    """
+    ok(is_running(), 'Process is not running.')
+
+
+def assert_not_running():
+    """
+    Assert that the test daemon is not running.
+    """
+    ok(not is_running(), 'Process is running.')
+
+
+class BasicService(Service):
+    """
+    Service base class.
+
+    Sets the service name and uses a temporary directory for PID files
+    to avoid the necessity of root privileges.
+    """
+    def __init__(self):
+        super(BasicService, self).__init__(_NAME, pid_dir='/tmp')
+
+
+class TimedService(BasicService):
+    """
+    Test service that runs for a certain amount of time.
+    """
+    def __init__(self, duration):
+        super(TimedService, self).__init__()
+        self.duration = duration
+
+    def run(self):
+        time.sleep(self.duration)
+
+
+class WaitingService(BasicService):
+    """
+    Test service that waits until shutdown via SIGTERM.
+    """
+    def __init__(self):
+        super(WaitingService, self).__init__()
+        self.event = threading.Event()
+
+    def run(self):
+        self.event.wait()
+
+    def on_terminate(self):
+        self.event.set()
+
+
+class ForeverService(BasicService):
+    """
+    A service that runs forever.
+    """
+    def run(self):
+        while True:
+            time.sleep(1)
+
+    on_terminate = run
+
+
+def start(service):
+    """
+    Start a service and wait until it's running.
+    """
+    service.start()
+    time.sleep(0.1)
+    assert_running()
+    return service
+
+
+class TestService(object):
+    """
+    Tests for ``service.Service``.
+    """
+
+    def setup(self):
+        service = BasicService()
+        try:
+            service.kill()
+        except ValueError:
+            pass
+
+    teardown = setup
+
+    def test_start(self):
+        """
+        Test ``Service.start``.
+        """
+        start(TimedService(0.2))
+
+    def test_stop(self):
+        """
+        Test ``Service.stop``.
+        """
+        service = start(WaitingService())
+        service.stop()
+        time.sleep(0.1)
+        assert_not_running()
+
+    def test_kill(self):
+        """
+        Test ``service.kill``.
+        """
+        service = start(ForeverService())
+        service.kill()
+        assert_not_running()
+
