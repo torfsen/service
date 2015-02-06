cbe0628cf0b855d55af2b9823200701361b44cf9
Torf <mail@torf.cc>
2015-02-06 20:51:18 +0100

    Bumped version to 0.1.1.

---
+++ CHANGES.rst
@@ -0,0 +1,5 @@
+:0.1.1:
+    * Improved error handling
+
+:0.1.0:
+    * Initial release.
--- src/service/__init__.py
+++ src/service/__init__.py
@@ -51,7 +51,7 @@ import lockfile.pidlockfile
 import setproctitle
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = ['Service']
 
