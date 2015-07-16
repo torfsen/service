3370c5b8b4dee554e84a630c478883a2188700cc
Torf <mail@torf.cc>
2015-07-16 21:27:10 +0200

    Updated Python versions in `setup.py`.

--- setup.py
+++ setup.py
@@ -73,7 +73,13 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 
